35b46f8815be843bac3a14a92906564a231d0e3b
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-02-06 20:20:48 +0100

    changed vite.config.js origin https url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // Check if the app is running in production
 const isProduction = process.env.NODE_ENV === 'production';
@@ -8,15 +8,19 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 4173,
     cors: {
-      origin: isProduction ? 'https://website-backend-1-n3p3.onrender.com' : 'http://localhost:3000',
+      origin: isProduction
+        ? 'https://website-frontend-8wnm.onrender.com/'
+        : 'http://localhost:3000',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: '*',
     },
     proxy: {
       '/api': {
-        target: isProduction ? 'https://api.render.com/deploy/srv-cs4kis52ng1s739mt85g?key=2n5SKhcxcQc' : 'http://localhost:3000',
+        target: isProduction
+          ? 'https://api.render.com/deploy/srv-cs4kis52ng1s739mt85g?key=2n5SKhcxcQc'
+          : 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Adjust path if needed
       },
