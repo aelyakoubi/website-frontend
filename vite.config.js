c91d8378044314757ad0a63d68e35a5187460d65
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2026-05-05 13:40:55 +0200

    modified:   .env modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,38 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// Check if the app is running in production
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     cors: {
-      origin: isProduction
-        ? 'https://website-backend-1-n3p3.onrender.com'
-        : 'http://localhost:3000',
+      origin: 'http://localhost:3000',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: '*',
     },
     proxy: {
       '/api': {
-        target: isProduction
-          ? 'https://api.render.com/deploy/srv-cs4kis52ng1s739mt85g?key=2n5SKhcxcQc'
-          : 'http://localhost:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Adjust path if needed
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
   preview: {
-    allowedHosts: ['website-frontend-8wnm.onrender.com'],
+    allowedHosts: [
+      'ivory-dugong-883765.hostingersite.com',
+      'mintcream-aardvark-942303.hostingersite.com',
+    ],
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          // Customize chunk splitting if needed
           vendor: ['react', 'react-dom'],
         },
       },
     },
     chunkSizeWarningLimit: 600,
   },
-  // No need to define process.env.NODE_ENV, use import.meta.env
 });
